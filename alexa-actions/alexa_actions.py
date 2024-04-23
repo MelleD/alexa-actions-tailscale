9c8b29f6c95ce0099fefef7124d646018e734b17
Dennis Melzer <dennis.melzer@de.bosch.com>
2024-04-23 18:44:46 +0200

    Update alexa_actions.py

--- alexa-actions/alexa_actions.py
+++ alexa-actions/alexa_actions.py
@@ -1,3 +1,11 @@
+#  Copyright (c) 2023.
+#  All rights reserved to the creator of the following script/program/app, please do not
+#  use or distribute without prior authorization from the creator.
+#  Creator: Antonio Manuel Nunes Goncalves
+#  Email: amng835@gmail.com
+#  LinkedIn: https://www.linkedin.com/in/antonio-manuel-goncalves-983926142/
+#  Github: https://github.com/DEADSEC-SECURITY
+
 # VERSION 0.0.1
 
 """ NO NEED TO EDIT ANYTHING UNDER THE LINE """
